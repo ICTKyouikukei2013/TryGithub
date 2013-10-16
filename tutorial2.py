3a91139a24ab2ea857774abf0a76b063b48c1118
b1012187 <b1012187@fun.ac.jp>
2013-10-16 13:19:14 +0900

    tutorial2.pyの宿題の部分をやりました

--- tutorial2.py
+++ tutorial2.py
@@ -1,14 +1,43 @@
 #coding: utf-8
 
 import sys
+import datetime
 
 from flask import Flask
 app = Flask(__name__)
 
-#@app.route("/")
-#def hello():
-#    return "Hello World!"
+"""
+@app.route('/<username>')は
+ http://a2c.bitbucket.org/flask/quickstart.html
+の変数ルールってところを見る。
+型を指定しないとおそらくstring型になる
 
+キャストの仕方は
+ (変換後の型)(値や変数)
+とする。値や変数が1個だったとしても()でくくる。
+
+日本語を含んだ文字列はASCⅡの文字コードと認識されるらしく、そのままusernameをぶち込むとエラーが起きるので、文字列の最初にuをつけて文字コードがutf-8だと認識させる
+ u"~~~"
+"""
+@app.route('/<username>')
+def show_username(username):
+	now = datetime.datetime.now()
+	random = (int)(now.microsecond) % 3
+	if(random == 0):
+		return 'Hello, %s!' % username
+	elif(random == 1):
+		return u'こんにちは、%s。' % username
+	else:
+		return u'%sさん、今日の日付は%sです。' % (username, now.strftime('%Y/%m/%d'))
+
+"""
+if __name__ == "__main__":
+    app.run()
+は一番下にないと@app.routeが反応しなかった
+
+500 internal server error
+app.runn()にdebug=Trueを渡すとエラーが出た時にどのへんがダメなのか教えてくれる
+"""
 if __name__ == "__main__":
     app.run()
 
